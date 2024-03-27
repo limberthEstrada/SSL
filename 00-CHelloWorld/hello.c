2872000c6106631e68e89937c2da6e3c4496fcac
limberthEstrada <lvasquezestrada@frba.utn.edu.ar>
2024-03-27 20:56:06 -0300

    Corrección luego de leer CharacterInputOutputRedirection.pdf

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -5,30 +5,10 @@
  */
 
 #include <stdio.h>
-void filecopy(FILE *ifp, FILE *ofp)
-{
-    int c;
-
-    while((c = getc(ifp)) != EOF)
-    {
-        putc(c, ofp);
-    }
-}
 
 int main(void){
-    FILE *fp;
-    fp = fopen("output.txt", "w");
 
-    if(fp == NULL){
-        printf("No se puede abrir");
-        return 1;
-    }
-    
-    freopen("output.txt", "w", stdout);
-    
     printf("Hello, World!\n");
-
-    fclose(fp);
-
+    
     return 0;
 }
